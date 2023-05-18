fa84406dba580546988e623fe9dc87f9649365a5
Iura ttttt <iurik1989@gmail.com>
2023-05-18 19:11:25 +0300

    Initial commit

---
+++ app/src/main/java/com/example/wether/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.wether
+
+import android.app.DownloadManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.example.wether.databinding.ActivityMainBinding
+import com.example.wether.fragment.MainFragment
+import org.json.JSONObject
+
+const val API_KEY = "dcc638b9622d4a668c4133433231805"
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        binding.button.setOnClickListener {
+           supportFragmentManager
+               .beginTransaction().replace(R.id.placeholder, MainFragment.newInstance()).commit()
+
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/wether/fragment/MainFragment.kt
@@ -0,0 +1,33 @@
+package com.example.wether.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.wether.R
+import com.example.wether.databinding.FragmentMainBinding
+
+
+
+class MainFragment : Fragment() {
+    private lateinit var binding: FragmentMainBinding
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentMainBinding.inflate(inflater,container, false)
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+
+    companion object {
+
+        @JvmStatic
+        fun newInstance() = MainFragment()
+
+
+    }
+}
